b355d49858de2a788a7a947781d599321b89cad8
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-04 22:58:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
   console.log('hello')
   return string.toLowerCase()
 }
-function logWhisper(sayHiToGrandma(string)) {
+function sayHiToGrandma(string) {
   console.log('hello')
   return ("I can't hear you!")
 }
